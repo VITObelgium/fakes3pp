3b4bd49a6409a7b4897d3cfdc148ac8913d39b83
Peter Van Bouwel <peter.vanbouwel@vito.be>
2025-06-25 15:15:57 +0200

    feature: allow specifying capabilities for S3 backends #27
    At this time it only adds support for 1 capability "StreamingUnsignedPayloadTrailer" which allows to make sure of S3 providers which do support this type of payload. By default this type of interaction is blocked as alternative implementations often do not support this type of interaction

---
+++ aws/service/s3/interfaces/backend-s3-capabilities.go
@@ -0,0 +1,19 @@
+package interfaces
+
+
+type S3Capability int
+
+//go:generate stringer -type=S3Capability -trimprefix=S3Capability $GOFILE
+
+//Below are the different supported capabilities. When specifying them in the config file
+//just omit the S3Capability part for example "StreamingUnsignedPayloadTrailer"
+const (
+	//Whether the payload of type STREAMING-UNSIGNED-PAYLOAD-TRAILER is
+	//supported by the backend details can be find in issue
+	//https://github.com/VITObelgium/fakes3pp/issues/27
+	S3CapabilityStreamingUnsignedPayloadTrailer S3Capability = iota
+)
+
+var S3CapabilityLookup map[string]S3Capability = map[string]S3Capability{
+	S3CapabilityStreamingUnsignedPayloadTrailer.String(): S3CapabilityStreamingUnsignedPayloadTrailer,
+}
